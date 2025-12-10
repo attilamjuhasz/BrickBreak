ae51226542f3d292af04c367e999da6b6a5f317f
Attila Juhasz <106558542+attilamjuhasz@users.noreply.github.com>
2025-12-10 16:06:37 +0000

    Working

--- src/main/java/com/BrickBreak/ Paddle.java
+++ src/main/java/com/BrickBreak/ Paddle.java
@@ -11,11 +11,49 @@ public class Paddle {
 
 	
 	//don't forget you need instance variables:
-	
+	private int x;
+	private int y;
+	private int velocity;
+	private int width;
+	private int height;
 	
 	//constructor(s):
+	public Paddle(int x, int y, int width, int height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
 	
 	//methods:
+
+//
+//  Pre-condition: Velocity must be defined
+//  Post-condition: Return int velocity
+//
+	public int getVelocity(){
+		return velocity;
+	}
+
+	public void move(){
+		x += velocity;
+	}
+
+//
+//  Pre-condition: A paddle object must be created
+//  Post-condition: Velocity will be greated by the given value
+//
+	public void addVelocity(int velocity){
+		this.velocity +=velocity;
+	}
+
+//
+//  Pre-condition: A paddle object must be created
+//  Post-condition: X coordinate will be eqaul to the given value
+//
+	public void setX(int x){
+		this.x = x;
+	}
 	
 	
 }
